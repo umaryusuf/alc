df169adf277dcb60a6d0afaa719ff202a77e78c8
umaryusuf <umaryusuf602@gmail.com>
2018-06-27 13:03:12 +0100

    second Commit

--- js/app.js
+++ js/app.js
@@ -1,31 +1,77 @@
+(function() {
 
+  // Select elements from the dom
+  const form = document.querySelector('#myForm');
+  const amount = form.querySelector('#amount');
+  const output = form.querySelector('#output');
+  const from = form.querySelector('#from');
+  const to = form.querySelector('#to');
+  const btn = form.querySelector('#convert');
 
-// Select elements from the dom
-// const amount = parseFloat(document.querySelector('#amount'));
-// const output = document.querySelector('#output');
-// let from = document.querySelector('#from');
-// let to = document.querySelector('#to');
+  // If the browser doesn't support service worker,
+  // we don't care about having a database
+  // exchange rate = (Math.round(currency*getAmount() * 100) / 100)
 
+  // {
+  //   "USD_PHP": {
+  //   "val": 53.519717
+  //   }
+  // }
 
-const convertCurrency = (amount, from, to, cb) => {
-  from = encodeURIComponent(from);
-  to = encodeURIComponent(to);
-  const query = `${from}_${to}`;
+  const dbPromise = idb.open('currencies-db', 1, function(upgradeDb) {
+    let store = upgradeDb.createObjectStore('currencies');
+    //store.createIndex('by-query');
+  });
 
-  const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=y`;
+  const currencyUrl = 'https://free.currencyconverterapi.com/api/v5/currencies';
+  fetch(currencyUrl)
+    .then(res => res.json())
+    .then( data => {
+      let temp = '';
 
-  fetch(url)
-    .then(res => {
-      return res.json();
+      Object.values(data.results).map(currency => {
+        temp += `
+          <option value="${currency['id']}">${currency['id']} - ${currency['currencyName']}</option>
+        `;
+      });
+
+      document.querySelector('#from').innerHTML = temp;
+      document.querySelector('#to').innerHTML = temp;
     })
-    .then(data => {
-      console.log(data[query]);
-    })
-    .catch(e => {
-      cb(e)
-    })
-}
+    .catch(err => console.log(err));
 
-convertCurrency(100, 'USD', 'NGN');
+  const convertCurrency = (amount, from, to, cb) => {
+    from = encodeURIComponent(from);
+    to = encodeURIComponent(to);
+    const query = `${from}_${to}`;
 
+    const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=y`;
+
+    fetch(url)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        let val = (amount * data[query].val);
+        console.log(val);
+        output.innerHTML = val;
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
+  btn.addEventListener('click', () => {
+    let error = false;
+
+    if(amount.value == '') {
+      // display amount is empty
+      alert('amount is empty');
+    } else {
+      convertCurrency(parseInt(amount.value), from.value, to.value);
+    }
+
+  });
+
+})();
 
--- sw.js
+++ sw.js
@@ -1,22 +1,38 @@
-const cacheName = 'v1'
+const staticName = 'converter-v1'
 self.addEventListener('install', event => {
   event.waitUntill(
-    caches.open(cacheName).then( cache => {
+    caches.open(staticName).then( cache => {
       return cache.addAll([
-        '/index.html',
-        '/css/bootstrap.min.css',
-        '/css/styles.css',
-        '/js/app.js'
+        'index.html',
+        'css/bootstrap.min.css',
+        'css/styles.css',
+        'js/app.js',
+        'js/idb.js',
+        'https://free.currencyconverterapi.com/api/v5/currencies'
       ]);
     })
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntill(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('converter-') && cacheName !== staticName;
+        }).map(cacheName => {
+          caches.delete(cacheName)
+        })
+      )
+    })
+  )
+})
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then( resp => {
       return resp || fetch(event.request).then( response => {
-        return caches.open(cacheName).then(function(cache) {
+        return caches.open(staticName).then(function(cache) {
           cache.put(event.request, response.clone());
           return response;
         });  
