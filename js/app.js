e381f3d03bbcbba4d9fbc82af802956ef80dcb6a
umaryusuf <umaryusuf602@gmail.com>
2018-06-29 15:15:10 +0100

    Added ofline conversion

--- js/app.js
+++ js/app.js
@@ -6,21 +6,11 @@
   const output = form.querySelector('#output');
   const from = form.querySelector('#from');
   const to = form.querySelector('#to');
+  const error = form.querySelector('#error');
   const btn = form.querySelector('#convert');
 
-  // If the browser doesn't support service worker,
-  // we don't care about having a database
-  // exchange rate = (Math.round(currency*getAmount() * 100) / 100)
-
-  // {
-  //   "USD_PHP": {
-  //   "val": 53.519717
-  //   }
-  // }
-
-  const dbPromise = idb.open('currencies-db', 1, function(upgradeDb) {
-    let store = upgradeDb.createObjectStore('currencies');
-    //store.createIndex('by-query');
+  const dbPromise = idb.open('currencies', 1, function(upgradeDb) {
+    let store = upgradeDb.createObjectStore('rates', { keyPath: 'pair' });
   });
 
   const currencyUrl = 'https://free.currencyconverterapi.com/api/v5/currencies';
@@ -43,30 +33,69 @@
   const convertCurrency = (amount, from, to, cb) => {
     from = encodeURIComponent(from);
     to = encodeURIComponent(to);
-    const query = `${from}_${to}`;
+    const from_to = `${from}_${to}`;
+    const to_from = `${to}_${from}`;
+    //check from indexDB
+    dbPromise.then(function(db) {
+      let tx = db.transaction('rates');
+      let keyValStore = tx.objectStore('rates');
+      return keyValStore.get(from_to);
+    }).then(function(val) {
+      // check if it exist in rates db
+      if(val) {
+        // exist in db
+        let total = (amount * val.rate);
+        
+        // display the result
+        output.innerHTML = total;
+      } else {
+        // go online and fetch from the api
+        const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${from_to},${to_from}&compact=ultra`;
 
-    const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=y`;
+        fetch(url)
+          .then(res => {
+            return res.json();
+          })
+          .then(data => {
 
-    fetch(url)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        let val = (amount * data[query].val);
-        console.log(val);
-        output.innerHTML = val;
-      })
-      .catch(e => {
-        console.log(e)
-      })
+            if(data) {
+              let total;
+        
+              total = (amount * data[from_to]);
+              // = Math.round(total * 100) / 100;
+
+              //store in indexDB
+              dbPromise.then(db => {
+                let tx = db.transaction('rates', 'readwrite');
+                let ratesStore = tx.objectStore('rates');
+
+                ratesStore.put({ pair: from_to, rate: data[from_to] });
+                ratesStore.put({ pair: to_from, rate: data[to_from] });
+
+                return tx.complete;
+              }).then(() => {
+                console.log('Rates saved to database');
+              });
+
+              // display the result
+              output.innerHTML = total;
+            }
+          })
+          .catch(e => {
+            console.log(e)
+          }
+        )
+      }
+    });
+   
   }
 
   btn.addEventListener('click', () => {
-    let error = false;
 
     if(amount.value == '') {
       // display amount is empty
-      alert('amount is empty');
+      error.innerHTML = 'Amount is empty';
+      amount.classList.add('is-invalid');
     } else {
       convertCurrency(parseInt(amount.value), from.value, to.value);
     }
